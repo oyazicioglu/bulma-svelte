3ce852acc6f7cea7724e52abff5a29aed365cfb4
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-06-22 02:38:44 +0300

    Update Dropdown

--- src/components/dropdown/dropdown-item.svelte
+++ src/components/dropdown/dropdown-item.svelte
@@ -1,35 +1,41 @@
 <script>
-  /** @type { string } */
-  export let href = undefined;
+    /** @type { string } */
+    export let href = undefined;
 
-  /** @type {Boolean}*/
-  export let isDivider = false;
+    /** @type {Boolean}*/
+    export let isDivider = false;
 
-  /** @type {Boolean}*/
-  export let asLink = true;
+    /** @type {Boolean}*/
+    export let asLink = true;
 
-  /**@type { CallableFunction }*/
-  export let action = undefined;
+    /**@type { CallableFunction }*/
+    export let action = undefined;
 
-  $: classes = [`dropdown-item`, $$restProps.class].filter(Boolean).join(' ');
-  $: styles = [$$restProps.style].filter(Boolean).join(';');
+    $: classes = [`dropdown-item`, $$restProps.class].filter(Boolean).join(' ');
+    $: styles = [$$restProps.style].filter(Boolean).join(';');
 </script>
 
 {#if isDivider}
-  <hr class="dropdown-divider" />
+    <hr class="dropdown-divider" />
 {:else if asLink}
-  <a {href} {...$$restProps} class={classes} style={styles}>
-    <slot />
-  </a>
+    <a {href} {...$$restProps} class={classes} style={styles}>
+        <slot />
+    </a>
 {:else}
-  <div
-    {...$$restProps}
-    class={classes}
-    style={styles}
-    on:click
-    on:click={() => {
-      action();
-    }}>
-    <slot />
-  </div>
+    <div
+        {...$$restProps}
+        class={classes}
+        style={styles}
+        on:click
+        on:click={() => {
+            action();
+        }}>
+        <slot />
+    </div>
 {/if}
+
+<style>
+    .dropdown-item {
+        cursor: pointer;
+    }
+</style>
--- src/components/dropdown/dropdown.svelte
+++ src/components/dropdown/dropdown.svelte
@@ -1,62 +1,70 @@
 <script>
-  import { hideOnClickOutside } from '../hideOnClickOutside';
-  import Icon from '../icon/icon.svelte';
-  import '../index.d.js';
+    import { hideOnClickOutside } from '../hideOnClickOutside';
+    import Icon from '../icon/icon.svelte';
+    import '../index.d.js';
 
-  /** @type {boolean}*/
-  export let isActive = false;
+    /** @type {boolean}*/
+    export let isActive = false;
 
-  /** @type { BulmaSize } */
-  export let size = 'is-normal';
+    /** @type { BulmaSize } */
+    export let size = 'is-normal';
 
-  /** @type {boolean}*/
-  export let isHoverable = false;
+    /** @type {boolean}*/
+    export let isHoverable = false;
 
-  /** @type {boolean}*/
-  export let isRight = false;
+    /** @type {boolean}*/
+    export let isRight = false;
 
-  /** @type {boolean}*/
-  export let isUp = false;
+    /** @type {boolean}*/
+    export let isUp = false;
 
-  /** @type {string}*/
-  export let text = 'Dropdown';
+    /** @type {string}*/
+    export let text = 'Dropdown';
 
-  /** @type {string}*/
-  export let iconName = 'angle-down';
+    /** @type {string}*/
+    export let iconName = 'angle-down';
 
-  $: classes = [
-    `dropdown`,
-    size,
-    isRight && `is-right`,
-    isUp && `is-up`,
-    isHoverable && `is-hoverable`,
-    isActive && `is-active`,
-    $$restProps.class,
-  ]
-    .filter(Boolean)
-    .join(' ');
+    /** @type {Boolean}*/
+    export let borderless = true;
 
-  $: styles = [$$restProps.style].filter(Boolean).join(';');
+    $: classes = [
+        `dropdown`,
+        size,
+        isRight && `is-right`,
+        isUp && `is-up`,
+        isHoverable && `is-hoverable`,
+        isActive && `is-active`,
+        $$restProps.class,
+    ]
+        .filter(Boolean)
+        .join(' ');
+
+    $: styles = [$$restProps.style].filter(Boolean).join(';');
 </script>
 
 <div
-  use:hideOnClickOutside={() => {
-    isActive = false;
-  }}
-  {...$$restProps}
-  class={classes}
-  style={styles}>
-  <div class="dropdown-trigger">
-    <button
-      class="button"
-      aria-haspopup="true"
-      aria-controls="dropdown-menu"
-      on:click={() => {
-        isActive = !isActive;
-      }}>
-      <span>{text}</span>
-      <Icon {size} {iconName} />
-    </button>
-  </div>
-  <slot />
+    use:hideOnClickOutside={() => {
+        isActive = false;
+    }}
+    {...$$restProps}
+    class={classes}
+    style={styles}>
+    <div class="dropdown-trigger">
+        <button
+            class="button"
+            class:is-white={borderless}
+            aria-haspopup="true"
+            aria-controls="dropdown-menu"
+            on:click={() => {
+                isActive = !isActive;
+            }}>
+            {#if text}
+                <span>{text}</span>
+            {/if}
+            {#if iconName}
+                <Icon {size} {iconName} />
+            {/if}
+        </button>
+    </div>
+    <slot />
 </div>
